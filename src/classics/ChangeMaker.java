4ab007e3f0ba5b72471b081b8953a0cc9a159f3f
cassioss <cassiosantossousa@gmail.com>
2015-09-04 15:23:53 -0300

    Additions and comments to ChangeMaker

--- src/classics/ChangeMaker.java
+++ src/classics/ChangeMaker.java
@@ -1,6 +1,7 @@
 package classics;
 
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 /**
  * Basic code for a change maker. Exactly as asked by DropBox. Instead of coins, the problem states for soda bottles. If
@@ -22,35 +23,54 @@ public class ChangeMaker {
      * @return an array list of lists containing all the unique combinations of packs that return sodasNumber.
      */
     public static ArrayList<ArrayList<Integer>> sodas(int sodasNumber, int[] packs) {
-        ArrayList<ArrayList<Integer>> allCombos = new ArrayList<ArrayList<Integer>>();
-        allCombos.addAll(checkSodas(sodasNumber, packs, new ArrayList<Integer>(), 0));
+        ArrayList<ArrayList<Integer>> allCombos = new ArrayList<>();
+        allCombos.addAll(checkSodas(sodasNumber, packs, new ArrayList<>(), 0));
         return allCombos;
     }
 
+    /**
+     * Safe copy of a list.
+     *
+     * @param list a list being copied.
+     * @return a second list with the same items, but different references in the memory.
+     */
     public static ArrayList<Integer> copyFrom(ArrayList<Integer> list) {
-        ArrayList<Integer> newList = new ArrayList<Integer>();
-        for (Integer i : list)
-            newList.add(i);
-        return newList;
+        return (ArrayList<Integer>) list.stream().collect(Collectors.toList());
     }
 
-    public static ArrayList<ArrayList<Integer>> checkSodas(int sodasNumber, int[] packs, ArrayList<Integer> currentCombo, int currentIndex) {
-        ArrayList<ArrayList<Integer>> partialCombos = new ArrayList<ArrayList<Integer>>();
+    public static ArrayList<ArrayList<Integer>> checkSodas(int sodasNumber, int[] packs,
+                                                           ArrayList<Integer> currentCombo, int currentIndex) {
+
+        ArrayList<ArrayList<Integer>> partialCombos = new ArrayList<>();
+
         if (sodasNumber == 0) {
-            partialCombos.add(currentCombo);
+            partialCombos.add(currentCombo); // No remaining sodas, the combo is over.
             return partialCombos;
         } else {
+
             if (sodasNumber < 0 || currentIndex == packs.length)
-                return partialCombos; //Empty list
+                return partialCombos; //Empty list. Do not add any list of packs. End this hook.
+
             else {
-                // First part: picking up the current pack
-                ArrayList<Integer> newCombo = copyFrom(currentCombo);
-                newCombo.add(packs[currentIndex]);
-                partialCombos.addAll(checkSodas(sodasNumber - packs[currentIndex], packs, newCombo, currentIndex));
-                // Second part: checking if the next pack can be taken
-                partialCombos.addAll(checkSodas(sodasNumber, packs, currentCombo, currentIndex + 1));
-                // Third part: getting them all together
-                return partialCombos;
+
+                // Second check
+                if(currentIndex >= packs.length)
+                    return partialCombos;
+
+                else {
+
+                    // First part: picking up the current pack
+                    ArrayList<Integer> newCombo = copyFrom(currentCombo);
+                    newCombo.add(packs[currentIndex]);
+                    partialCombos.addAll(checkSodas(sodasNumber - packs[currentIndex], packs, newCombo, currentIndex));
+
+                    // Second part: checking if the next pack can be taken
+                    partialCombos.addAll(checkSodas(sodasNumber, packs, currentCombo, currentIndex + 1));
+
+                    // Third part: getting them all together
+                    return partialCombos;
+
+                }
             }
         }
     }
@@ -67,7 +87,7 @@ public class ChangeMaker {
         Boolean first = true;
         for (ArrayList<Integer> combo : sodasCombo) {
             if (!first)
-                System.out.print(",");
+                System.out.print(",\n");
             else
                 first = false;
             System.out.print(combo.toString());
@@ -81,7 +101,7 @@ public class ChangeMaker {
      * @param args all arguments of the main function - none are required.
      */
     public static void main(String[] args) {
-        int[] packs = new int[]{0, 2, 4, 6, 9};
+        int[] packs = new int[]{3,4,6};
         printAllCombinations(13, packs);
     }
 }
